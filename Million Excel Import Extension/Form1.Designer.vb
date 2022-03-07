e2bd8c6756d688347dd90c940a930ca34cc350cb
RBADM07 <RBADM07@DESKTOP-RTJGC3I>
2022-03-07 16:16:52 +0800

    7-3-2022 4:16PM

--- Million Excel Import Extension/Form1.Designer.vb
+++ Million Excel Import Extension/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Good = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -28,7 +28,17 @@ Partial Class Form1
         Me.btnFileOpen = New System.Windows.Forms.Button()
         Me.lblFileLocation = New System.Windows.Forms.Label()
         Me.btnSql = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblSQLStatus = New System.Windows.Forms.Label()
+        Me.DataGridViewTables = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridViewTables, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
         Me.SuspendLayout()
         '
         'DataGridView1
@@ -36,15 +46,17 @@ Partial Class Form1
         Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Good})
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 57)
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(776, 338)
+        Me.DataGridView1.Size = New System.Drawing.Size(400, 350)
         Me.DataGridView1.TabIndex = 0
         '
         'Good
         '
         Me.Good.HeaderText = "Column1"
         Me.Good.Name = "Good"
+        Me.Good.Width = 327
         '
         'OpenFileDialog1
         '
@@ -52,7 +64,7 @@ Partial Class Form1
         '
         'btnFileOpen
         '
-        Me.btnFileOpen.Location = New System.Drawing.Point(12, 12)
+        Me.btnFileOpen.Location = New System.Drawing.Point(3, 3)
         Me.btnFileOpen.Name = "btnFileOpen"
         Me.btnFileOpen.Size = New System.Drawing.Size(117, 23)
         Me.btnFileOpen.TabIndex = 1
@@ -62,7 +74,7 @@ Partial Class Form1
         'lblFileLocation
         '
         Me.lblFileLocation.AutoSize = True
-        Me.lblFileLocation.Location = New System.Drawing.Point(172, 17)
+        Me.lblFileLocation.Location = New System.Drawing.Point(126, 8)
         Me.lblFileLocation.Name = "lblFileLocation"
         Me.lblFileLocation.Size = New System.Drawing.Size(16, 13)
         Me.lblFileLocation.TabIndex = 2
@@ -70,27 +82,94 @@ Partial Class Form1
         '
         'btnSql
         '
-        Me.btnSql.Location = New System.Drawing.Point(251, 11)
+        Me.btnSql.Location = New System.Drawing.Point(485, 3)
         Me.btnSql.Name = "btnSql"
         Me.btnSql.Size = New System.Drawing.Size(147, 23)
         Me.btnSql.TabIndex = 3
-        Me.btnSql.Text = "Get From SQL"
+        Me.btnSql.Text = "Connect to SQL Server"
         Me.btnSql.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(638, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Status: "
+        '
+        'lblSQLStatus
+        '
+        Me.lblSQLStatus.AutoSize = True
+        Me.lblSQLStatus.BackColor = System.Drawing.Color.Transparent
+        Me.lblSQLStatus.ForeColor = System.Drawing.Color.DarkRed
+        Me.lblSQLStatus.Location = New System.Drawing.Point(687, 8)
+        Me.lblSQLStatus.Name = "lblSQLStatus"
+        Me.lblSQLStatus.Size = New System.Drawing.Size(73, 13)
+        Me.lblSQLStatus.TabIndex = 5
+        Me.lblSQLStatus.Text = "Disconnected"
+        '
+        'DataGridViewTables
+        '
+        Me.DataGridViewTables.AllowUserToAddRows = False
+        Me.DataGridViewTables.AllowUserToDeleteRows = False
+        Me.DataGridViewTables.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells
+        Me.DataGridViewTables.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridViewTables.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridViewTables.Name = "DataGridViewTables"
+        Me.DataGridViewTables.ReadOnly = True
+        Me.DataGridViewTables.Size = New System.Drawing.Size(398, 350)
+        Me.DataGridViewTables.TabIndex = 6
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnFileOpen)
+        Me.Panel1.Controls.Add(Me.lblFileLocation)
+        Me.Panel1.Controls.Add(Me.lblSQLStatus)
+        Me.Panel1.Controls.Add(Me.btnSql)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 100)
+        Me.Panel1.TabIndex = 7
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.DataGridViewTables)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel2.Location = New System.Drawing.Point(402, 100)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(398, 350)
+        Me.Panel2.TabIndex = 8
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.DataGridView1)
+        Me.Panel3.Location = New System.Drawing.Point(0, 100)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(400, 350)
+        Me.Panel3.TabIndex = 9
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.btnSql)
-        Me.Controls.Add(Me.lblFileLocation)
-        Me.Controls.Add(Me.btnFileOpen)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Million Excel Import Extension"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridViewTables, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -100,4 +179,10 @@ Partial Class Form1
     Friend WithEvents lblFileLocation As Label
     Friend WithEvents Good As DataGridViewTextBoxColumn
     Friend WithEvents btnSql As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblSQLStatus As Label
+    Friend WithEvents DataGridViewTables As DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
 End Class
--- Million Excel Import Extension/Form1.vb
+++ Million Excel Import Extension/Form1.vb
@@ -1,61 +1,94 @@
 ﻿Imports System.Data
 Imports System.Data.OleDb
 Imports System.Data.SqlClient
+Imports ExcelDataReader
 Public Class Form1
+    Dim deviceName As String = Environment.MachineName
+    Dim myConn As SqlConnection
+    Dim tableName As String
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
     Private Sub btnFileOpen_Click(sender As Object, e As EventArgs) Handles btnFileOpen.Click
-        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
-            Dim cn As OleDbConnection
-            Dim cmd As OleDbCommand
-            Dim da As OleDbDataAdapter
-            Dim dt As DataTable
-            da = New OleDbDataAdapter
-            dt = New DataTable
-            Dim fileLocation = OpenFileDialog1.FileName
-            Debug.Write("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileLocation +
-                                     ";Extension Properties='Excel 8.0; IMEX=1; HDR=Yes;'")
-            'cn = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileLocation +
-            '                         ";Extension Properties='Excel 8.0; IMEX=1; HDR=Yes;'")
-            cn = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileLocation +
-                                     ";Extended Properties='Excel 8.0; IMEX=1; HDR=Yes;'")
+        'https://www.youtube.com/watch?v=pQ1PpcIcHno
+        Using ofd As OpenFileDialog = New OpenFileDialog() With {.Filter = "Excel 97-2003 Workbook|*.xls|Excel Workbook|*.xlsx"}
+            If ofd.ShowDialog() = DialogResult.OK Then
+                lblFileLocation.Text = ofd.FileName
+                Using stream = File.Open()
+            End If
+        End Using
 
-            cmd = New OleDbCommand("select * from [sheet1$]", cn)
-            da.SelectCommand = cmd
-            da.Fill(dt)
 
-            DataGridView1.DataSource = dt
-            lblFileLocation.Text = OpenFileDialog1.FileName
 
-        End If
+
+        'If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+        '    Dim cn As OleDbConnection
+        '    Dim cmd As OleDbCommand
+        '    Dim da As OleDbDataAdapter
+        '    Dim dt As DataTable
+        '    da = New OleDbDataAdapter
+        '    dt = New DataTable
+        '    Dim fileLocation As String = OpenFileDialog1.FileName
+        '    Dim fileExtension As String = New IO.FileInfo(fileLocation).Extension
+        '    If (fileExtension.Equals(".xlsx")) Then
+        '        'cn = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileLocation +
+        '        '                     ";Extended Properties='Excel 12.0; IMEX=1; HDR=Yes;'")
+        '        MsgBox("here")
+        '        cn = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileLocation + ";Extended Properties=Excel 12.0;")
+        '    Else
+        '        cn = New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileLocation +
+        '                             ";Extended Properties='Excel 8.0; IMEX=1; HDR=Yes;'")
+        '    End If
+        '    cmd = New OleDbCommand("select * from [sheet1$]", cn)
+        '    da.SelectCommand = cmd
+        '    da.Fill(dt)
+
+        '    DataGridView1.DataSource = dt
+        '    lblFileLocation.Text = OpenFileDialog1.FileName
+
+        'End If
     End Sub
 
     Private Sub btnSql_Click(sender As Object, e As EventArgs) Handles btnSql.Click
-        Dim myConn As SqlConnection
-        Dim myCmd As SqlCommand
-        Dim myReader As SqlDataReader
-        Dim results As String
-        'Create a Connection object.
-        myConn = New SqlConnection("Initial Catalog=Northwind;" &
-        "Data Source=DESKTOP-GAUUIC1;Initial Catalog=test;Integrated Security=True")
-        'Create a Command object.
-        myCmd = myConn.CreateCommand
-        myCmd.CommandText = "SELECT name, description FROM Product"
-
+        Try
+            lblSQLStatus.ForeColor = Color.Green
+            lblSQLStatus.Text = "Connected"
+            tableName = "sys.Tables"
+            bindGrid(tableName, "name")
+        Catch ex As SqlException
+            MsgBox(ex.Message, MsgBoxStyle.Critical, Me.Text)
+        End Try
+    End Sub
+    Private Sub bindGrid(table As String, rows As String)
+        myConn = New SqlConnection("Data Source=" + deviceName + "\ML001; " &
+                                   "Initial Catalog=demo;user id=sa;password=<ml$a>")
         'Open the connection.
         myConn.Open()
-        myReader = myCmd.ExecuteReader()
-        'Concatenate the query result into a string.
-        Do While myReader.Read()
-            results = results & myReader.GetString(0) & vbTab &
-            myReader.GetString(1) & vbLf
-        Loop
-        'Display results.
-        MsgBox(results)
-        'Close the reader and the database connection.
-        myReader.Close()
+        Using myConn
+            Using cmd As New SqlCommand("SELECT " + rows + " FROM " + table, myConn)
+                cmd.CommandType = CommandType.Text
+                Using sda As New SqlDataAdapter(cmd)
+                    Using ds As New DataSet()
+                        sda.Fill(ds)
+                        DataGridViewTables.DataSource = ds.Tables(0)
+                    End Using
+                End Using
+            End Using
+        End Using
+        tableName = table
         myConn.Close()
+    End Sub
+
+    Private Sub DataGridViewTables_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridViewTables.CellDoubleClick
+        If (e.RowIndex <> -1 And e.ColumnIndex <> -1) Then
+            Dim cellValue = DataGridViewTables.CurrentRow.Cells(e.ColumnIndex).Value.ToString
+            If tableName.Equals("sys.Tables") Then
+                bindGrid(cellValue, "*")
+            End If
+        End If
+
+
     End Sub
 End Class
